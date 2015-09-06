b211d58c3de5ac59e36827183e515b81f09bb47a
brenj <brenj@mailbox.org>
2015-09-05 19:34:46 -0700

    Add solution to exercise 4, planetproto

---
+++ node-school/planetproto/040.js
@@ -0,0 +1,21 @@
+var machine = {
+  motors: null
+};
+var robot = {};
+var vehicle= {};
+
+vehicle.__proto__ = machine;
+robot.__proto__ = machine;
+claim(machine.motors, null);
+claim(robot.motors, null);
+claim(vehicle.motors, null);
+robot.motors = 4;
+claim(machine.motors, null);
+claim(robot.motors, 4);
+claim(vehicle.motors, null);
+
+module.exports = {
+  machine: machine,
+  vehicle: vehicle,
+  robot: robot
+};
