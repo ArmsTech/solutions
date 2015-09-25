c60ea0d8df8e2c2c962a01487609eb4a6c86b9be
brenj <brenj@mailbox.org>
2015-09-25 06:38:10 -0700

    Updated solution to exercise 4, containers
    This commit adds some more code (though not complete) to the solution to
    exercise 4, containers.

--- hacker-rank/python/containers/piling_up.py
+++ hacker-rank/python/containers/piling_up.py
@@ -15,24 +15,26 @@ for _ in range(int(raw_input())):
     max_length = int(raw_input())
     deck = deque(map(int, raw_input().split()), max_length)
 
-    last_length = -1
-    passes_test = True
+    last_stacked = None
 
-    for _ in range(len(deck) / 2):
-        try:
-            left_most = deck.popleft()
-        except IndexError:
-            left_most = -1
+    for _ in range(int(round(len(deck) / float(2)))):
+        left_most = deck.popleft()
 
         try:
             right_most = deck.pop()
         except IndexError:
-            right_most = -1
-
-        if left_most >= last_length or right_most >= last_length:
-            passes_test = False
+            passes_test = left_most <= last_stacked
             break
 
-        last_length = left_most if left_most <= right_most else right_most
+        first_to_stack = max(left_most, right_most)
+        second_to_stack = min(left_most, right_most)
+
+        passes_test = second_to_stack <= first_to_stack <= (
+            last_stacked or first_to_stack)
+
+        if not passes_test:
+            break
+
+        last_stacked = second_to_stack
 
     print 'Yes' if passes_test else 'No'
