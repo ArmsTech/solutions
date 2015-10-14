80c4bcc589401bd0c5f06ff79eb5820d7064453b
brenj <brenj@mailbox.org>
2015-10-14 16:59:39 -0500

    Add solution to challenge 8, bash

---
+++ hacker-rank/linux-shell/bash/more-on-conditionals.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# More On Conditionals Challenge
+
+read x
+read y
+read z
+
+if (( "${x}" == "${y}" && "${x}" == "${z}" )); then
+  echo "EQUILATERAL"
+elif (( "${x}" == "${y}" || "${y}" == "${z}" || "${x}" == "${z}" )); then
+  echo "ISOSCELES"
+else
+  echo "SCALENE"
+fi
