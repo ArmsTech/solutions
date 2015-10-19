4cc4ce51ac78d91f713b6d48d878ba30c077c4f7
brenj <brenj@mailbox.org>
2015-10-19 15:06:04 -0500

    Add solution to challenge 31, text-processing

---
+++ hacker-rank/linux-shell/text-processing/paste3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Paste #3 Challenge
+
+# Given a CSV file where each row contains the name of a city and its state
+# separated by a comma, your task is to replace the newlines in the file with
+# tabs as demonstrated in the sample.
+
+paste -d$'\t' -s -
