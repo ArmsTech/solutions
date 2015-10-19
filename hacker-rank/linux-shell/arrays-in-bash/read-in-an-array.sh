329ad16ceeeac313fe405528d6c6fb66645c96c1
brenj <brenj@mailbox.org>
2015-10-19 16:40:25 -0500

    Add solution to challenge 1, arrays-in-bash

---
+++ hacker-rank/linux-shell/arrays-in-bash/read-in-an-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Read In An Array Challenge
+
+# Given a list of countries, each on a new line, your task is to read them
+# into an array and then display the entire array, with a space between each
+# of the countries' names.
+
+countries=$(cat)
+echo ${countries[@]}
