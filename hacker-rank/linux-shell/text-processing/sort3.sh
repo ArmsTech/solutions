cf6832c4f4199d56f5476e3a50a260f549bbb7c5
brenj <brenj@mailbox.org>
2015-10-19 10:31:56 -0500

    Add solution to challenge 20, text-processing

---
+++ hacker-rank/linux-shell/text-processing/sort3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Sort #3 Challenge
+
+# You are given a file of text, where each line contains a number (which may
+# be either an integer or have decimal places). There will be no extra
+# characters other than the number or the newline at the end of each line.
+# Sort the lines in ascending order - such that the first line holds the
+# (numerically) smallest number and the last line hold the (numerically)
+# largest number.
+
+sort -n -
