1d9358ceb8f7288c395c38f2687678d3b10e6f91
brenj <brenj@mailbox.org>
2015-10-19 14:51:46 -0500

    Add solution to challenge 29, text-processing

---
+++ hacker-rank/linux-shell/text-processing/paste1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Paste #1 Challenge
+
+# Given a CSV file where each row contains the name of a city and its state
+# separated by a comma, your task is to replace the newlines in the file with
+# semicolons as demonstrated in the sample.
+
+paste -d';' -s -
