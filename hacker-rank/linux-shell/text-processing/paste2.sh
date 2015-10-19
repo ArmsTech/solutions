efa24b131d65abda13df70f1315f58f1eaf2ade5
brenj <brenj@mailbox.org>
2015-10-19 14:57:08 -0500

    Add solution to challenge 30, text-processing

---
+++ hacker-rank/linux-shell/text-processing/paste2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Paste #2 Challenge
+
+# Given a CSV file where each row contains the name of a city and its state
+# separated by a comma, your task is to restructure the file in such a way,
+# that three consecutive rows are folded into one, and separated by
+# semi-colons.
+
+paste -d';' - - - --
