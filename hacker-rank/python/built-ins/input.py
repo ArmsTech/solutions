9245f2fb731fab9074fae027bfe64a8c35f12816
brenj <brenj@mailbox.org>
2016-01-18 11:08:05 -0600

    Add solution 3, python built-ins

---
+++ hacker-rank/python/built-ins/input.py
@@ -0,0 +1,8 @@
+# Input Challenge
+"""
+You are given a polynomial P of a single indeterminate (or variable), x.
+You are also given the values of x and k. Your task is to verify if P(x)=k
+"""
+
+x, k = map(int, raw_input().split())
+print input() == k
