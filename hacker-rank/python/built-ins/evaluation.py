b392cebde110f42648e5cdbceefd098859b869e4
brenj <brenj@mailbox.org>
2016-01-18 11:15:47 -0600

    Add solution 4, python built-ins

---
+++ hacker-rank/python/built-ins/evaluation.py
@@ -0,0 +1,9 @@
+# Evaluation Challenge
+"""
+You are given an expression in a line. Read that line as a string variable,
+such as var, and print the result using eval(var).
+"""
+
+from __future__ import print_function
+
+eval(raw_input())
