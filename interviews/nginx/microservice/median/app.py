b7c062b796260d54d62db22e85b7d675006d9c9e
brenj <brenj@mailbox.org>
2016-03-12 21:56:52 -0600

    Get integers from the last minute

--- interviews/nginx/microservice/median/app.py
+++ interviews/nginx/microservice/median/app.py
@@ -6,6 +6,9 @@ import falcon
 
 from median.persistence import redis_store, INTEGERS_SET_KEY
 
+# Seconds
+EPOCH_MINUTE = 60
+
 
 class IntegerResource(object):
 
@@ -16,7 +19,7 @@ class IntegerResource(object):
         integer = req.get_param_as_int('integer', required=True)
 
         elements_added = redis_store.zadd(
-            INTEGERS_SET_KEY, integer, time.time())
+            INTEGERS_SET_KEY, time.time(), integer)
         added_successfully = elements_added == 1
 
         resp.status = (
@@ -29,7 +32,11 @@ class MedianResource(object):
 
     def on_get(self, req, resp):
         """Handle requests for the median in the last minute."""
-        resp.body = "GET median"
+        epoch_time = time.time()
+        integers = redis_store.zrangebyscore(
+            'integers', epoch_time - EPOCH_MINUTE, epoch_time)
+
+        resp.body = str(integers)
         resp.status = falcon.HTTP_200
 
 
