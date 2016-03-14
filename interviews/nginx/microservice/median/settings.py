503cf96dc3db5200f2ffe6cc69b610497b3a16e8
brenj <brenj@mailbox.org>
2016-03-13 20:46:39 -0500

    Add a settings module

---
+++ interviews/nginx/microservice/median/settings.py
@@ -0,0 +1,7 @@
+# Celery
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_TASK_RESULT_EXPIRES = 60
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
